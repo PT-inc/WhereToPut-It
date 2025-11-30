6ad47c52be4441cfc8e2e638a2e09b9485dd8ec9
Trevor Chartier <trevor.chartier@colostate.edu>
2025-11-30 14:13:13 -0700

    Create Distance Calculator  Function as a UDF

---
+++ src/main/java/cs455/amazon/DistanceCalculatorUDF.java
@@ -0,0 +1,28 @@
+package cs455.amazon;
+
+import org.apache.spark.sql.api.java.UDF4;
+
+public class DistanceCalculatorUDF implements UDF4<Double, Double, Double, Double>{
+    @Override
+    public Double call(Double lat1, Double lng1, Double lat2, Double lng2) throws Exception{
+        final Double earthRadius = 3958.8;
+
+        Double lat1Radians = Math.toRadians(lat1);
+        Double lng1Radians = Math.toRadians(lng1);
+
+        Double lat2Radians = Math.toRadians(lat2);
+        Double lng2Radians = Math.toRadians(lng2);
+
+        Double dlng = lng2Radians - lng1Radians;
+        Double dlat = lat2Radians - lat1Radians;
+
+        Double a = Math.pow(Math.sin(dlat / 2), 2) 
+            + Math.cos(lat1Radians) * Math.cos(lat2Radians) * Math.pow(Math.sin(dlng / 2), 2);
+
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        Double dist = earthRadius * c;
+
+        return dist;
+    }
+}
