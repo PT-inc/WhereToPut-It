438856e01ba20bac92ea41d1c7e1ae1a0f5d95c6
Trevor Chartier <trevor.chartier@colostate.edu>
2025-12-02 17:20:53 -0700

    Build controller to coordinate candidate set creation and passign to optimizer.

--- src/main/java/cs455/amazon/Controller.java
+++ src/main/java/cs455/amazon/Controller.java
@@ -1,32 +1,37 @@
 package cs455.amazon;
  
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
 import cs455.amazon.BaselineCalculator;
 import cs455.amazon.Optimizer;
  
 public class Controller {
     public static void main(String[] args) {
         if(args.length != 5){
-            System.err.println("Usage: Controller <amazonCentersPath> <demandRegionsPath> <candidateLocationsPath> <baselineOutputPath> <optimizerOutputPath>");
+            System.err.println("Usage: Controller <amazonCentersPath> <demandRegionsPath> <baselineOutputPath> <optimizerOutputPath> <numCandidates>");
             System.exit(1);
         }
  
         String amazonCentersPath = args[0];
         String demandRegionsPath = args[1];
-        String candidateLocationsPath = args[2];
-        String baselineOutputPath = args[3];
-        String optimizerOutputPath = args[4];
+        String baselineOutputPath = args[2];
+        String optimizerOutputPath = args[3];
+        int numCandidates = Integer.valueOf(args[4]);
  
         SparkSession sc = SparkSession.builder()
                                 .appName("WhereToPut-It")
                                 .getOrCreate();
  
         BaselineCalculator baseline = new BaselineCalculator(sc, amazonCentersPath, demandRegionsPath, baselineOutputPath);
-        baseline.run();
+        Dataset<Row> baselineDf = baseline.run();
+
+        CandidateGenerator generator = new CandidateGenerator(sc, demandRegionsPath);
+        Dataset<Row> candidateDf = generator.generateCandidateDf(numCandidates);
  
-        Optimizer optimizer = new Optimizer(sc, candidateLocationsPath, baselineOutputPath, optimizerOutputPath);
+        Optimizer optimizer = new Optimizer(sc, candidateDf, baselineDf, optimizerOutputPath);
         optimizer.run();
- 
  
         sc.stop();
     }
